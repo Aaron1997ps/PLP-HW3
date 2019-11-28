c270fc4e826e778a60cf8c7ae178e5549288627c
paperprincessai <34976339+paperprincessai@users.noreply.github.com>
2019-11-27 22:11:35 -0600

    attempting to figure out how to evaluate +

--- HW4/listexpression.cpp
+++ HW4/listexpression.cpp
@@ -142,6 +142,9 @@ int main(int argc, char** argv) {
 
     string line, originalLine;
     string vari, data, type;
+    stringstream checking(data);
+    string temp;
+    vector<string> words;
     char cdata;
     variableStorage variables;
 
@@ -217,7 +220,16 @@ int main(int argc, char** argv) {
                     type = variables.getType(data);
                     data = variables.getValue(data);
                 }
+                if(line.find('+')){
+                
+                while(getline(checkit, temp, '+')){
+                    if(checkit.getValue(temp)){
+                        words.clear();
+                        words.push_back(temp);
 
+                         //still need to search through this and append data with proper stuffs
+
+                    }
                 variables.setVariable(vari, type, data);
             }else{
                 string dataIf, dataElse, boolExpression;
